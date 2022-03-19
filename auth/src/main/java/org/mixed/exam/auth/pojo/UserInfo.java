70006d87013996c8d26c0541db5239ac7a31b30c
Administrator <2213772647@qq.com>
2022-03-19 21:21:06 +0800

    用户小属性改变

--- auth/src/main/java/org/mixed/exam/auth/pojo/UserInfo.java
+++ auth/src/main/java/org/mixed/exam/auth/pojo/UserInfo.java
@@ -9,8 +9,8 @@ import lombok.NoArgsConstructor;
 @Data
 public class UserInfo
 {
-    private Integer id;
     private String username;
     private String password;
     private String role;
+    private String realName;
 }
