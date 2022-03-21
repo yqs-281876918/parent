289d79ce8bb94e8cedfa1c1a7aa6703ff3d23f41
yqs <281876918@qq.com>
2022-03-21 14:11:15 +0800

    yml

--- paper/src/test/java/org/mixed/exam/paper/service/IntelligentServiceTest.java
+++ paper/src/test/java/org/mixed/exam/paper/service/IntelligentServiceTest.java
@@ -1,19 +1,23 @@
 package org.mixed.exam.paper.service;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.mixed.exam.paper.pojo.dto.IntelligentParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
+@SpringBootTest
 public class IntelligentServiceTest {
     @Test
     public void testIndividual()
     {
-        Individual individual = new Individual(31);
+        IntelligentService.Individual individual = new IntelligentService.Individual(31);
         individual.setGene(28,1);
         individual.setGene(17,1);
         individual.printGene();
@@ -48,7 +52,21 @@ public class IntelligentServiceTest {
     {
         IntelligentService service = new IntelligentService();
         IntelligentParam param =new IntelligentParam();
-        param.setSubjectCount(30);
         service.build(param);
+    }
+    @Autowired
+    private IntelligentService intelligentService;
+    @Test
+    public void createChinesePaper()
+    {
+        IntelligentParam param = new IntelligentParam();
+        param.setDifficulty(3.5);
+        param.setCourseID("622dbb040913ed05f6402c8b");
+        param.setKeys(Arrays.asList("622dcb956fdcc85405ef85ad", "623074271aabf7331f816aad", "62304bf274a387398923fe3b"));
+        List<IntelligentParam.SubjectDistribution> distributions = new ArrayList<>();
+        distributions.add(new IntelligentParam.SubjectDistribution("singleChoiceQuestion",5));
+        distributions.add(new IntelligentParam.SubjectDistribution("readComprehension",3));
+        distributions.add(new IntelligentParam.SubjectDistribution("completion",8));
+        intelligentService.build(param);
     }
 }
