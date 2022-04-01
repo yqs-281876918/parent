bf7484b9d2d93063a3173b6011e1a941a1e10610
Administrator <2213772647@qq.com>
2022-04-01 19:58:15 +0800

    bank的api中加入新建的表

---
+++ bank-api/src/main/java/org/mixed/exam/bank/api/pojo/po/AnswerDetail.java
@@ -0,0 +1,8 @@
+package org.mixed.exam.bank.api.pojo.po;
+
+import lombok.Data;
+
+@Data
+public abstract class AnswerDetail {
+
+}
---
+++ bank-api/src/main/java/org/mixed/exam/bank/api/pojo/po/ExamDetail.java
@@ -0,0 +1,19 @@
+package org.mixed.exam.bank.api.pojo.po;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExamDetail {
+    private String id;
+    private String username; //用户名
+    private Integer examId; //考试Id
+    private List<Answer> answers; //学生作答情况
+    private Integer totalScore = -1; //总分数
+    private Integer antiCount = 0; //切屏次数
+}
