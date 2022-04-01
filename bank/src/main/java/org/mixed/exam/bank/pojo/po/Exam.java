d6c7e8a3ef3c02b22fadc8fce01c7dbaae2f25d8
Administrator <2213772647@qq.com>
2022-04-01 12:49:56 +0800

    考试类中增加试卷分数的属性

--- bank/src/main/java/org/mixed/exam/bank/pojo/po/Exam.java
+++ bank/src/main/java/org/mixed/exam/bank/pojo/po/Exam.java
@@ -9,12 +9,14 @@ public class Exam
     @Id
     private Integer id;
     private String paperID;
+    private String scoreList;//每道题目的分数列表
+    private Integer totalScore;//试卷的总分
     private String examName;
     private Integer testTime;
     private Long startTime;
     private Integer lateTime;
     private Integer submitTime;
-    private String classID;
+    private Integer classID;
     private String[] antiSettings;
     private String introduce;
 }
