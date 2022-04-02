dc8261cbf51a279853679cf759dc4968cc89f0e2
yqs <281876918@qq.com>
2022-04-02 16:15:08 +0800

    删除题型

--- bank-api/src/main/java/org/mixed/exam/bank/api/pojo/po/exam/Answer.java
+++ bank-api/src/main/java/org/mixed/exam/bank/api/pojo/po/exam/Answer.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,5 +13,5 @@ public class Answer {
     private String subjectId;
     private String subjectType;
     private Integer score = -1;
-    private AnswerDetail answerDetail;
+    private List<String> answerList;
 }
