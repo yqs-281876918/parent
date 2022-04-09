68dfba3b95781649949c7609c38d3b3900386e76
zhx <1454791185@qq.com>
2022-04-09 16:27:29 +0800

    test

--- teacher/src/test/java/org/mixed/exam/teacher/dao/AnalyseDaoTest.java
+++ teacher/src/test/java/org/mixed/exam/teacher/dao/AnalyseDaoTest.java
@@ -3,16 +3,22 @@ package org.mixed.exam.teacher.dao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class AnalyseDaoTest {
-
+    @Autowired
+    private MongoTemplate mongoTemplate;
     @Autowired
     private AnalyseDao analyseDao;
     @Test
     void getStuList() {
         System.out.println(analyseDao.getStuList(13));
+    }
+    @Test
+    public void find(){
+
     }
 }
