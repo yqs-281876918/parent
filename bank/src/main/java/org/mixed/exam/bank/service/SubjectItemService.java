d5f73427552feea3c179a6c401139f5a8def94fd
Administrator <2213772647@qq.com>
2022-04-15 11:48:56 +0800

    题库管理界面完善

--- bank/src/main/java/org/mixed/exam/bank/service/SubjectItemService.java
+++ bank/src/main/java/org/mixed/exam/bank/service/SubjectItemService.java
@@ -1,5 +1,7 @@
 package org.mixed.exam.bank.service;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageInfo;
 import org.mixed.exam.bank.dao.SubjectDao;
 import org.mixed.exam.bank.pojo.po.Question;
 import org.mixed.exam.bank.pojo.vo.SubjectItem;
@@ -28,5 +30,30 @@ public class SubjectItemService
             }
         }
         return items;
+    }
+    public PageInfo<SubjectItem> getSubjectItems(Integer pageNum, Integer pageSize){
+        List<SubjectItem> items=new ArrayList<>();
+        List<String> typeList = SubjectUtil.getTypeList();
+        for(String type : typeList)
+        {
+            List<? extends Question> list = subjectDao.getSubjectsByType(type);
+            for(Question q : list)
+            {
+                items.add(new SubjectItem(q));
+            }
+        }
+        //创建Page类
+        Page page = new Page(pageNum, pageSize);
+        //为Page类中的total属性赋值
+        int total =items.size();
+        page.setTotal(total);
+        //计算当前需要显示的数据下标起始值
+        int startIndex = (pageNum - 1) * pageSize;
+        int endIndex = Math.min(startIndex + pageSize,total);
+        //从链表中截取需要显示的子链表，并加入到Page
+        page.addAll(items.subList(startIndex,endIndex));
+        //以Page创建PageInfo
+        PageInfo<SubjectItem> pageInfo = new PageInfo<>(page);
+        return pageInfo;
     }
 }
---
+++ bank/src/main/java/org/mixed/exam/bank/util/PageUtil.java
@@ -0,0 +1,13 @@
+package org.mixed.exam.bank.util;
+
+public class PageUtil {
+//    public static <T> Page<T> listConvertToPage(List<T> list, long total, Pageable pageable) {
+//        // 当前页第一条数据在List中的位置
+//        int start = (pageable.getPageNumber() - 1) * pageable.getPageSize();
+//        // 当前页最后一条数据在List中的位置
+//        int end = (start + pageable.getPageSize()) > list.size() ? list.size() : (pageable.getPageSize() * pageable.getPageNumber());
+//        //System.out.println(start + " , " + end + ", size:" + list.size());
+//        //配置分页数据
+//        return new Page<T>(list.subList(start, end), total, pageable);
+//    }
+}
